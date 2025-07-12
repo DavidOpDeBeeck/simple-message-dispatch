4317b5984d0b2c7ed27481f3cad06aa982b6291b
David Op de Beeck <davidopdebeeck@outlook.be>
2025-07-12 14:32:08 +0200

    Add publishing configuration to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     `maven-publish`
+    signing
 }
 
 group = "app.dodb"
@@ -9,10 +10,12 @@ version = "0.0.1"
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     java {
         sourceCompatibility = JavaVersion.VERSION_21
         withSourcesJar()
+        withJavadocJar()
     }
 
     dependencies {
@@ -54,11 +57,53 @@ subprojects {
                 groupId = rootProject.group.toString()
                 artifactId = project.name
                 version = rootProject.version.toString()
+
+                pom {
+                    name.set("Simple Message Dispatch")
+                    description.set("A lightweight Java library designed to support CQRS and event-driven applications.")
+                    url.set("https://github.com/DavidOpDeBeeck/simple-message-dispatch")
+                    inceptionYear.set("2025")
+                    licenses {
+                        license {
+                            name.set("GPL-3.0 license")
+                            url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("davidodb")
+                            name.set("David Op de Beeck")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/DavidOpDeBeeck/simple-message-dispatch.git")
+                        developerConnection.set("scm:git:ssh://github.com/DavidOpDeBeeck/simple-message-dispatch.git")
+                        url.set("https://github.com/DavidOpDeBeeck/simple-message-dispatch")
+                    }
+                }
             }
         }
 
         repositories {
             mavenLocal()
+            repositories {
+                maven {
+                    name = "sonatype"
+                    url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = findProperty("sonatypeUsername") as String
+                        password = findProperty("sonatypePassword") as String
+                    }
+                }
+            }
         }
+    }
+
+    signing {
+        useInMemoryPgpKeys(
+            findProperty("signing.key") as String?,
+            findProperty("signing.password") as String?
+        )
+        sign(publishing.publications["mavenJava"])
     }
 }
