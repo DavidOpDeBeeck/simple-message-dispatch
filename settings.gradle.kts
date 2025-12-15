2c906deda997ece336d4fb9173d20480bc67fb20
David Op de Beeck <davidopdebeeck@outlook.be>
2025-12-15 17:29:42 +0100

    Introduce folder structure for core and framework

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,14 @@
 rootProject.name = "simple-message-dispatch"
 
 include("smd-api")
+include("smd-test")
 include("smd-spring-boot-starter")
 include("smd-spring-boot-starter-test")
-include("smd-test")
+
+project(":smd-api").projectDir = file("core/smd-api")
+project(":smd-test").projectDir = file("core/smd-test")
+project(":smd-spring-boot-starter").projectDir = file("framework/smd-spring-boot-starter")
+project(":smd-spring-boot-starter-test").projectDir = file("framework/smd-spring-boot-starter-test")
 
 dependencyResolutionManagement {
     repositories {
